0332511ff64bce158c6956d28b02aa5fdac93612
Afan Dayu Laksono <afandayul@gmail.com>
2022-09-10 22:08:30 +0700

    add brand image

---
+++ src/components/Brand.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Images from '../assets/Images.svg'
+
+const Brand = () => {
+    return (
+        <div className='brand'>
+            <img src={Images} />
+        </div>
+    )
+}
+
+export default Brand
--- src/pages/FindDealer.js
+++ src/pages/FindDealer.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import Brand from '../components/Brand'
 import Navbar from '../components/Navbar'
 
 const FindDealer = () => {
     return (
         <>
             <Navbar />
+            <Brand />
         </>
     )
 }
