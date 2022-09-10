83aeaf4117aa04db4b42cd38756572ea02066728
Afan Dayu Laksono <afandayul@gmail.com>
2022-09-10 21:31:45 +0700

    fix style navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,19 +2,20 @@ import React from 'react'
 import Logo from '../assets/Logo.svg'
 import MidLogo from '../assets/midLogo.svg'
 import Menu from '../assets/Menu.svg'
+import { ReactComponent as Vector } from '../assets/Vector.svg'
 
 const Navbar = () => {
     return (
         <nav className='container'>
-            <ul>
-                <img src={Logo} alt='Logo' />
-            </ul>
+
+            <img className='logo' src={Logo} alt='Logo' />
+
             <ul className="list">
                 <img src={MidLogo} alt='MidLogo' />
-                <li className="items">ABOUT US</li>
-                <li className="items">EXPLORE CARS</li>
-                <li className="items">PROMO</li>
-                <li className="items">NEWS & EVENTS</li>
+                <li className="items">ABOUT US  </li><Vector />
+                <li className="items">EXPLORE CARS</li><Vector />
+                <li className="items">PROMO</li><Vector />
+                <li className="items">NEWS & EVENTS</li><Vector />
                 <img src={Menu} alt='Menu' />
             </ul>
         </nav>
