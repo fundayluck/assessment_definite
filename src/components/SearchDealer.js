af2fc9e8c4a7204e9c46ed4df5b8ce5843ca777c
Afan Dayu Laksono <afandayul@gmail.com>
2022-09-11 14:31:25 +0700

    fix content

--- src/components/SearchDealer.js
+++ src/components/SearchDealer.js
@@ -1,8 +1,73 @@
 import React from 'react'
+import { ReactComponent as Pointermap } from '../assets/pointermap.svg'
 
+const Data = [
+    {
+        nama: 'Srikandi Mampang',
+        alamat: 'Jl. Mampang Prapatan No. 21-23, Jakarta Selatan',
+    },
+    {
+        nama: 'Srikandi Mampang',
+        alamat: 'Jl. Mampang Prapatan No. 21-23, Jakarta Selatan',
+    },
+    {
+        nama: 'Srikandi Mampang',
+        alamat: 'Jl. Mampang Prapatan No. 21-23, Jakarta Selatan',
+    },
+    {
+        nama: 'Srikandi Mampang',
+        alamat: 'Jl. Mampang Prapatan No. 21-23, Jakarta Selatan',
+    },
+    {
+        nama: 'Srikandi Mampang',
+        alamat: 'Jl. Mampang Prapatan No. 21-23, Jakarta Selatan',
+    },
+    {
+        nama: 'Srikandi Mampang',
+        alamat: 'Jl. Mampang Prapatan No. 21-23, Jakarta Selatan',
+    },
+    {
+        nama: 'Srikandi Mampang',
+        alamat: 'Jl. Mampang Prapatan No. 21-23, Jakarta Selatan',
+    },
+    {
+        nama: 'Srikandi Mampang',
+        alamat: 'Jl. Mampang Prapatan No. 21-23, Jakarta Selatan',
+    },
+    {
+        nama: 'Srikandi Mampang',
+        alamat: 'Jl. Mampang Prapatan No. 21-23, Jakarta Selatan',
+    },
+]
 const SearchDealer = () => {
+
+
     return (
-        <div>SearchDealer</div>
+        <div className='container-search-dealer'>
+            <div className='search'>
+                <p>Discover the nearest dealership in your area</p>
+                <input type='text' />
+            </div>
+            <div className='container-card'>
+                {Data.map((item, index) =>
+                    <div className='card' key={index}>
+                        <Pointermap />
+                        <div className='card-item'>
+                            <h3>{item.nama}</h3>
+                            <p>{item.alamat}</p>
+                            <div>
+                                Sales • Service • Sparepart
+                            </div>
+                        </div>
+                    </div>
+                )}
+            </div>
+            <div className='btn'>
+                <button className='btn-dealer'>
+                    LOAD MORE
+                </button>
+            </div>
+        </div>
     )
 }
 
--- src/pages/FindDealer.js
+++ src/pages/FindDealer.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Brand from '../components/Brand'
 import Navbar from '../components/Navbar'
+import SearchDealer from '../components/SearchDealer'
 
 const FindDealer = () => {
     return (
         <>
             <Navbar />
             <Brand />
+            <SearchDealer />
         </>
     )
 }
