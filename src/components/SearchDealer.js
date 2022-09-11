3f9cf488e3a28b9571fefe90d6f7e3ff1e45a610
Afan Dayu Laksono <afandayul@gmail.com>
2022-09-11 22:29:40 +0700

    fetch api

--- src/components/SearchDealer.js
+++ src/components/SearchDealer.js
@@ -1,60 +1,45 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { ReactComponent as Pointermap } from '../assets/pointermap.svg'
+import axios from 'axios'
 
-const Data = [
-    {
-        nama: 'Srikandi Mampang',
-        alamat: 'Jl. Mampang Prapatan No. 21-23, Jakarta Selatan',
-    },
-    {
-        nama: 'Srikandi Mampang',
-        alamat: 'Jl. Mampang Prapatan No. 21-23, Jakarta Selatan',
-    },
-    {
-        nama: 'Srikandi Mampang',
-        alamat: 'Jl. Mampang Prapatan No. 21-23, Jakarta Selatan',
-    },
-    {
-        nama: 'Srikandi Mampang',
-        alamat: 'Jl. Mampang Prapatan No. 21-23, Jakarta Selatan',
-    },
-    {
-        nama: 'Srikandi Mampang',
-        alamat: 'Jl. Mampang Prapatan No. 21-23, Jakarta Selatan',
-    },
-    {
-        nama: 'Srikandi Mampang',
-        alamat: 'Jl. Mampang Prapatan No. 21-23, Jakarta Selatan',
-    },
-    {
-        nama: 'Srikandi Mampang',
-        alamat: 'Jl. Mampang Prapatan No. 21-23, Jakarta Selatan',
-    },
-    {
-        nama: 'Srikandi Mampang',
-        alamat: 'Jl. Mampang Prapatan No. 21-23, Jakarta Selatan',
-    },
-    {
-        nama: 'Srikandi Mampang',
-        alamat: 'Jl. Mampang Prapatan No. 21-23, Jakarta Selatan',
-    },
-]
 const SearchDealer = () => {
+    const [getDataProvinces, setGetDataProvinces] = useState([])
+    const [getDealer, setDealer] = useState([])
+    const provinces = getDataProvinces;
+    const dealer = getDealer
+
+
+
+    useEffect(() => {
+        const fetch = async () => {
+            const res = await axios.get('https://mitsubishi-50.sudahdistaging.in/api/frontend/get-provinces')
+            setGetDataProvinces(res.data)
+        }
+        const response = async () => {
+            const res = await axios.get('https://mitsubishi-50.sudahdistaging.in/api/frontend/search-dealers?page=1&limit=9')
+            setDealer(res.data.data)
+        }
+        fetch()
+        response()
+    }, [])
 
 
     return (
         <div className='container-search-dealer'>
             <div className='search'>
                 <p>Discover the nearest dealership in your area</p>
-                <input type='text' />
+                <select>
+                    <option>Pilih Lokasi Terdekat</option>
+                    {provinces.map((item, i) => <option value={item.name} key={i}>{item.name}</option>)}
+                </select>
             </div>
             <div className='container-card'>
-                {Data.map((item, index) =>
+                {dealer.map((item, index) =>
                     <div className='card' key={index}>
                         <Pointermap />
                         <div className='card-item'>
-                            <h3>{item.nama}</h3>
-                            <p>{item.alamat}</p>
+                            <h3>{item.title}</h3>
+                            <p>{item.address}</p>
                             <div>
                                 Sales • Service • Sparepart
                             </div>
