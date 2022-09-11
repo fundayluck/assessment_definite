c53163ef6aca39b15436a37727b833bacdc4adf0
Afan Dayu Laksono <afandayul@gmail.com>
2022-09-11 13:05:27 +0700

    adding brand

--- src/components/Brand.js
+++ src/components/Brand.js
@@ -3,9 +3,15 @@ import Images from '../assets/Images.svg'
 
 const Brand = () => {
     return (
-        <div className='brand'>
-            <img src={Images} />
-        </div>
+        <>
+            <div className='brand'>
+                <img src={Images} />
+            </div>
+            <div className='sub-brand'>
+                <h1 className='sub-brand-item-1'>FIND DEALER</h1>
+                <p className='sub-brand-item-2'>Cari dan kunjungi dealer resmi Mitsubishi terdekat di kota Anda untuk mendapatkan pelayanan terbaik terkait dengan kendaraan dari Mitsubishi Motors Indonesia.</p>
+            </div>
+        </>
     )
 }
 
---
+++ src/components/SearchDealer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SearchDealer = () => {
+    return (
+        <div>SearchDealer</div>
+    )
+}
+
+export default SearchDealer
